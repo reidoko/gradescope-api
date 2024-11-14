c494713cd065c5deae416fc74e1d1d6f8f773475
Rei <dokorei@msu.edu>
2024-11-14 17:19:21 -0500

    setup requirements

--- setup.py
+++ setup.py
@@ -5,16 +5,13 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="gradescope-api",
-    version="0.0.2",
-    author="CS 161 Staff",
-    author_email="cs161-staff@berkeley.edu",
+    version="0.0.3",
+    author="Rei Doko",
+    author_email="dokorei@msu.edu",
     description="An unofficial Gradescope API.",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/cs161-staff/gradescope-api",
-    project_urls={
-        "Homepage": "https://cs161.org",
-    },
+    url="https://github.com/reidoko/gradescope-api",
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
@@ -23,5 +20,5 @@ setuptools.setup(
     package_dir={"": "src"},
     packages=setuptools.find_packages(where="src"),
     python_requires=">=3.6",
-    install_requires=["requests", "bs4"],
+    install_requires=["requests", "bs4", "pytz"],
 )
